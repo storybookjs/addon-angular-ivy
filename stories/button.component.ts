dbde10d1b5297ec505c95bc8809414462bfa1551
Kai Röder <roeder.kai@gmail.com>
2021-04-21 12:35:39 +0200

    add directives for testing purposes

--- stories/button.component.ts
+++ stories/button.component.ts
@@ -1,21 +1,36 @@
-import { Component, NgModule } from "@angular/core";
+import { Component, Input, NgModule, OnInit } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatDividerModule } from "@angular/material/divider";
 import { MatIconModule } from "@angular/material/icon";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { BehaviorSubject } from "rxjs";
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: "sb-button",
   templateUrl: "button.component.html",
   styleUrls: ["button.component.scss"]
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit {
+
+  @Input() basicLabel = 'Basic';
+
+  isVisible$ = new BehaviorSubject(false);
+
+  ngOnInit() {
+    setTimeout(() => this.isVisible$.next(true), 200);
+  }
+
+  toggle() {
+    this.isVisible$.next(!this.isVisible$.value);
+  }
 }
 
 @NgModule({
   declarations: [ButtonComponent],
   imports: [
     BrowserAnimationsModule,
+    CommonModule,
     MatButtonModule,
     MatDividerModule,
     MatIconModule
