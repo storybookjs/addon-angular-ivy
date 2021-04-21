5461b60a37367773c3b28d4ca596465c2ff6ffe2
Kai Röder <roeder.kai@gmail.com>
2021-04-21 10:21:13 +0200

    add angular material

--- stories/button.component.ts
+++ stories/button.component.ts
@@ -1,17 +1,25 @@
 import { Component, NgModule } from "@angular/core";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatIconModule } from "@angular/material/icon";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @Component({
-  selector: 'sb-button',
-  template: `
-    <button>click me</button>
-  `,
-  styles: []
+  selector: "sb-button",
+  templateUrl: "button.component.html",
+  styleUrls: ["button.component.scss"]
 })
 export class ButtonComponent {
 }
 
 @NgModule({
   declarations: [ButtonComponent],
+  imports: [
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule
+  ],
   exports: [ButtonComponent]
 })
 export class ButtonModule {
